d1f69977e00461b2c54af89126e76da78922920b
Maxim <31155487+KozinM@users.noreply.github.com>
2023-05-02 14:03:35 +0300

    React Props
    example of using React's props
    minor:
     - List variable changed from global context to in App's context (name changed to stories)
     - new component appeared called Item

--- src/App.js
+++ src/App.js
@@ -1,68 +1,71 @@
 import * as React from "react";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-function App() {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <Search/>
+      <Search />
       <hr />
-      <List />
+      <List list={stories} />
       {/* render the list here */}
       {/* this is how comments in JSX are made */}
     </div>
   );
-}
+};
 
 export default App;
 
-function List() {
+const List = (props) => {
   return (
     <ul>
-      {list.map(function (item) {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href="{item.url}">{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        );
-      })}
+      {props.list.map((item) => (
+        <Item key={item.objectID} item={item} />
+      ))}
     </ul>
   );
-}
+};
+
+const Item = (props) => {
+  return (
+    <li>
+      <span>
+        <a href="{item.url}">{props.item.title}</a>
+      </span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
+    </li>
+  );
+};
 
 const Search = () => {
-
   const handleChange = (event) => {
     console.log(event.target.value);
-  }
+  };
 
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange}/>
+      <input id="search" type="text" onChange={handleChange} />
     </div>
   );
-}
+};
