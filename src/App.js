f6a2b4eec5edd9293a0056b44dc5166acd053774
Maxim <31155487+KozinM@users.noreply.github.com>
2023-05-10 21:43:17 +0300

    Inline Handler in JSX
    Moved stories to a global variable
    introduced removeHandler and made it an inline handler

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import * as React from "react";
 
+const initialStories = [
+  {
+    title: "React",
+    url: "https://reactjs.org",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 const App = () => {
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
 
   /*   const [searchTerm, setSearchTerm] = useState(
     localStorage.getItem('searchTerm')??"React");
@@ -40,6 +41,13 @@ const App = () => {
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
 
+  const [stories, setStories] = React.useState(initialStories);
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter((story)=> {return item.objectID !== story.objectID});
+    setStories(newStories);
+  };
+ 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
@@ -61,7 +69,7 @@ const App = () => {
         </InputWithLabel>
       <Button />
       <hr />
-      <List list={searchedStories} />
+      <List list={searchedStories}  onRemoveItem={handleRemoveStory}/>
       {/* render the list here */}
       {/* this is how comments in JSX are made */}
     </div>
@@ -70,25 +78,40 @@ const App = () => {
 
 export default App;
 
-const List = ({ list }) => {
+const List = ({ list, onRemoveItem }) => {
   return (
     <ul>
-      {list.map(({ objectID, ...item }) => (
-        <Item key={objectID} {...item} />
+      {list.map((item) => (
+        <Item 
+          key={item.objectID} 
+          item ={item}
+          onRemoveItem={onRemoveItem}
+          />
       ))}
     </ul>
   );
 };
 
-const Item = ({ title, url, author, num_comments, points }) => {
+const Item = ({ item, onRemoveItem }) => {
+/*   const handleRemoveItem = () => {
+    onRemoveItem(item);
+  }; */
+
   return (
     <li>
       <span>
-        <a href={url}>{title}</a>
+        <a href={item.url}>{item.title}</a>
       </span>
-      <span>{author}</span>
-      <span>{num_comments}</span>
-      <span>{points}</span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+      <span>
+        <button
+          type="button"
+          onClick={()=> onRemoveItem(item)}>
+            Dismiss
+        </button>
+      </span>
     </li>
   );
 };
